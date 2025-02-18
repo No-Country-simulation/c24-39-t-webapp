729cb2066ea33239e80d8fb87ec7d4bc3dc1c356
brianrojas <briaan.dc@gmail.com>
2025-02-18 18:08:33 -0300

    creacion de models user y restaurant

---
+++ backend/src/main/java/com/c24_39_t_webapp/restaurants/models/User.java
@@ -0,0 +1,48 @@
+package com.c24_39_t_webapp.restaurants.models;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String address;
+
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public enum UserRole {
+        CLIENT,
+        RESTAURANT
+    }
+
+
+}

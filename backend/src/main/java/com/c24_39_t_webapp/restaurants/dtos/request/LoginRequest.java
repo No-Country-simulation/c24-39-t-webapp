63bbb279236c1c120ba4ec621ed538b8de06d5c1
brianrojas <briaan.dc@gmail.com>
2025-02-23 14:20:53 -0300

    se ordena clases en segurity

--- backend/src/main/java/com/c24_39_t_webapp/restaurants/dtos/request/LoginRequest.java
+++ backend/src/main/java/com/c24_39_t_webapp/restaurants/dtos/request/LoginRequest.java
@@ -1,4 +1,4 @@
-package com.c24_39_t_webapp.restaurants.security;
+package com.c24_39_t_webapp.restaurants.dtos.request;
 
 public record LoginRequest(
         String email,
--- backend/src/main/java/com/c24_39_t_webapp/restaurants/services/impl/UserDetailsImpl.java
+++ backend/src/main/java/com/c24_39_t_webapp/restaurants/services/impl/UserDetailsImpl.java
@@ -1,15 +1,13 @@
-package com.c24_39_t_webapp.restaurants.security;
+package com.c24_39_t_webapp.restaurants.services.impl;
 
 import com.c24_39_t_webapp.restaurants.models.UserEntity;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 
 @AllArgsConstructor
 public class UserDetailsImpl implements UserDetails {

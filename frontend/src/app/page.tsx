12ed0f338a488a41cfab96ddabf36d18459074ff
brianrojas <briaan.dc@gmail.com>
2025-02-27 19:53:19 -0300

    cambios en el login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,16 @@
 import { Button } from "flowbite-react";
+import { auth } from "../../auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        {session?.user?.email ?? "no autenticado"}
         <Button>Clickk dale</Button>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        footer
-      </footer>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">footer</footer>
     </div>
   );
 }

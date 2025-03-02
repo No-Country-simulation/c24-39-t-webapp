765f0201fa555a1372d7fa84edf9e61348c01bf6
Javier Piñeyro <dariojavierpineyro@gmail.com>
2025-03-02 18:41:59 -0300

    Fix: Now Login and Register return user data

--- backend/src/main/java/com/c24_39_t_webapp/restaurants/dtos/response/UserResponseDto.java
+++ backend/src/main/java/com/c24_39_t_webapp/restaurants/dtos/response/UserResponseDto.java
@@ -1,6 +1,7 @@
 package com.c24_39_t_webapp.restaurants.dtos.response;
 
 import com.c24_39_t_webapp.restaurants.models.UserEntity;
+import com.c24_39_t_webapp.restaurants.services.impl.UserDetailsImpl;
 import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,6 +22,17 @@ public class UserResponseDto {
         this.id = user.getId();
         this.name = user.getName();
         this.email = user.getEmail();
+        this.role = user.getRole();
+        this.phone = user.getPhone();
+        this.address = user.getAddress();
+    }
+
+    // Sobrecarga del contructor para devolver en el login esta clase sin
+    // usar un UserEntity
+    public UserResponseDto(UserDetailsImpl user){
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getUsername();
         this.role = user.getRole();
         this.phone = user.getPhone();
         this.address = user.getAddress();

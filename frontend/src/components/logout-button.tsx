125d416f9789137b74fa4586153c67ae0be1633f
Javier Piñeyro <dariojavierpineyro@gmail.com>
2025-03-06 12:58:24 -0300

    Change styles of this component - remove bg

--- frontend/src/components/logout-button.tsx
+++ frontend/src/components/logout-button.tsx
@@ -5,7 +5,7 @@ import {signOut} from "next-auth/react"
 export default function LogoutButton() {
     return <button 
         onClick={()=> signOut({redirectTo: "/login"})}
-        className="bg-transparent:hover:bg-gray-200/50 transition-opacity text-black">
+        className="bg-transparent :hover:bg-gray-200/50 transition-opacity text-black">
         Cerrar sesión
     </button>
 }

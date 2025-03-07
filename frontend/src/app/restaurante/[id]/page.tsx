efac622e7c2ced9cb5a3ab83a75f54d07cac42fc
brianrojas <briaan.dc@gmail.com>
2025-03-07 16:37:18 -0300

    se genera la logica de agregar produtctos al localstore

--- frontend/src/app/restaurante/[id]/page.tsx
+++ frontend/src/app/restaurante/[id]/page.tsx
@@ -5,19 +5,19 @@ import Link from "next/link";
 import { HiArrowLeft } from "react-icons/hi";
 
 type Props = {
-  params: {
-    id: string;
-  }
-}
+  params: Promise<{ id: string }>;
+};
 
 export default async function Page({ params }: Props) {
-  console.log(`ID: ${params.id} es de tipo ${typeof params.id}`);
-  const { id } = params;
+  const { id } = await params;
+
+  // Obtener datos del restaurante desde la API
   const restaurant = await api.restaurant.get(Number(id));
-  //const products = await api.product.getAllByRestaurant(Number(id))
-	const products: Product[] = [
+
+  // Simulación de productos (cambiar por API real)
+  const products: Product[] = [
     {
-      "prd_id": 1,
+      prd_id: 1,
       name: "Hamburguesa",
       price: 3500,
       description: "Una hamburguesa",
@@ -28,7 +28,7 @@ export default async function Page({ params }: Props) {
       categoryId: 1,
     },
     {
-      "prd_id": 2,
+      prd_id: 2,
       name: "Pizza",
       price: 4000,
       description: "Una pizza",
@@ -38,72 +38,36 @@ export default async function Page({ params }: Props) {
       isActive: true,
       categoryId: 2,
     },
-    {
-      "prd_id": 3,
-      name: "Ensalada",
-      price: 2500,
-      description: "Una ensalada",
-      restaurantId: Number(id),
-      quantity: 100,
-      image: "https://cdn.pixabay.com/photo/2016/09/15/19/24/salad-1439859_960_720.jpg",
-      isActive: true,
-      categoryId: 3,
-    },
-    {
-      "prd_id": 4,
-      name: "Pizza de pasta",
-      price: 4800,
-      description: "Una pizza de pasta con queso en la orilla",
-      restaurantId: Number(id),
-      quantity: 100,
-      image: "https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg",
-      isActive: true,
-      categoryId: 2,
-    },
-    {
-      "prd_id": 5,
-      name: "Pizza de doble queso",
-      price: 4800,
-      description: "Una pizza de doble queso con relleno en la orilla",
-      restaurantId: Number(id),
-      quantity: 100,
-      image: "https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg",
-      isActive: true,
-      categoryId: 2,
-    }
-  ]
+  ];
 
   return (
-    <main
-      className="min-h-screen w-full md:w-[760px] text-black p-2 md:p-6 mx-auto flex 
-		  flex-col align-center shadow-lg"
-    >
+    <main className="min-h-screen w-full md:w-[760px] text-black p-2 md:p-6 mx-auto flex flex-col align-center shadow-lg relative">
       <div>
         <Link
-          className="flex gap-1 w-fit items-center justify-center transition-colors 
-			hover:bg-gray-200/60 hover:border-black/80 px-2 py-1 text-black decoration-none  
-			rounded-full border border-black"
+          className="flex gap-1 w-fit items-center justify-center transition-colors hover:bg-gray-200/60 hover:border-black/80 px-2 py-1 text-black decoration-none rounded-full border border-black"
           href="/"
         >
           <HiArrowLeft className="size-3" />
           Volver
         </Link>
       </div>
-      <div className="w-full flex mb-2 justify-center items-center">
-        {/* eslint-disable-next-line @next/next/no-img-element*/}
-        <img
-          height="250px"
-          className="object-cover w-[50%] md:w-[30%]"
-          src={restaurant.logo}
-          alt={`Logo de ${restaurant.name}`}
-        />
-      </div>
-      <h1 className="leading-2 text-center text-2xl md:text-5xl font-bold">
-        {restaurant.name}
-      </h1>
-      <p className="text-center md:text-2xl text-black/70 ">
-        {restaurant.description}
-      </p>
+
+      {restaurant && (
+        <>
+          <div className="w-full flex mb-2 justify-center items-center">
+            <img
+              height="250px"
+              className="object-cover w-[50%] md:w-[30%]"
+              src={restaurant.logo}
+              alt={`Logo de ${restaurant.name}`}
+            />
+          </div>
+          <h1 className="leading-2 text-center text-2xl md:text-5xl font-bold">{restaurant.name}</h1>
+          <p className="text-center md:text-2xl text-black/70">{restaurant.description}</p>
+        </>
+      )}
+
+      {/* Menú (componente Client) */}
       <section className="mt-6">
         <Menu products={products} />
       </section>
